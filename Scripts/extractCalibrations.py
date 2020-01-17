b7beef5e7411c0ef3092d19d7aa3307720d3fb57
BOUSSAU Bastien <bastien.boussau@univ-lyon1.fr>
2020-01-17 20:44:00 +0100

    Outputting calibrations in a good format for revbayes. Need to improve the output for clades.

--- Scripts/extractCalibrations.py
+++ Scripts/extractCalibrations.py
@@ -50,15 +50,43 @@ def biasedWeights(d):
         weights[i] = weights[i]/float(sum_weights)
     return weights
 
+
+def outputCalibrations(clades, ages, fout2, fout, sentence):
+    index = 0
+    fout2.write("\n############"+ sentence)
+    fout2.write("####### Internal node calibrations #######\n")
+    fout.write("######### Clade file ##########")
+    for c in clades:
+        name_clade = "clade_"+str(index)
+        fout.write(name_clade+" = clade(")
+        start=True
+        for tax in c:
+            if start:
+                fout.write("\""+tax+"\"")
+                start=False
+            else:
+                fout.write(",\""+tax+"\"")
+        fout.write(")\n")
+        fout2.write("### we create a deterministic node for the age of the MRCA of each clade with a fossil calibration\n")
+        fout2.write("tmrca_"+name_clade+" := tmrca(psi,"+name_clade+")\n")
+        fout2.write("age_fossil_"+name_clade+" <- tmrca(psi,"+name_clade+")\n")
+        fout2.write("width_age_prior_"+name_clade+" <- tmrca_" + name_clade + " / 5  # By default we decide that the width of the calibration interval is fossil_age/5\n")
+        fout2.write("mean_age_prior_"+name_clade+" <- tmrca_" + name_clade + "\n")
+        fout2.write("obs_age_"+name_clade+" ~ dnSoftBoundUniformNormal(min=age_fossil_"+name_clade+" - width_age_prior_"+name_clade+", max=age_fossil_"+name_clade+" + width_age_prior_"+name_clade+", sd=2.5, p=0.95\n")
+        fout2.write("obs_age_"+name_clade+".clamp( mean_age_prior_"+name_clade +")\n" )
+        index = index+1
+
+
 ##########################################
 ############## The main function #########
 ##########################################
 if __name__ == "__main__":
     file = sys.argv[1]
-    out = file.split('.')[0]+'_calibrations.txt'
+    calibration_file = file.split('.')[0]+'_calibrations.Rev'
     num_cal = int(sys.argv[2])
     balanced = sys.argv[3] # Do we want to sample calibrations in a balanced or unbalanced way
     old = sys.argv[4] # Do we want to sample old calibrations rather than recent ones.
+    clade_file = file.split('.')[0]+'_clades.Rev'
 
     t = readTreeFromFile(file)
 
@@ -77,6 +105,7 @@ if __name__ == "__main__":
 
     t_begin = Tree()
 
+    # Balanced or not?
     if ('y' in balanced):
         # Getting calibrations from both sides of the root
         t_begin = t
@@ -86,17 +115,13 @@ if __name__ == "__main__":
         choice  = random.choice(choices)
         print("Choosing calibrations from subtree: ", choice)
         t_begin = t.get_children()[choice]
-
-    print("Number of nodes in t_begin: ", len(t_begin.get_descendants()))
+    print("Number of nodes in sampled subtree: ", len(t_begin.get_descendants()))
 
     id2Height = getInternalNodeHeights( t_begin )
     nodeId2LeafListRef, leafList2NodeIdRef, idToDescendants = getNameToLeavesAndIdToDescendantIdsLink( t_begin )
-
     # Let's order the nodes according to their heights:
     d_ascending = OrderedDict(sorted(id2Height.items(), key=lambda kv: kv[1]))
 
-    # print("d_ascending: ")
-    # print(d_ascending)
     calibrated_nodes_red = list()
     calibrated_nodes_blue = list()
 
@@ -109,7 +134,7 @@ if __name__ == "__main__":
                            replace=False,
                            p=weights)
     elif old == "both":
-        print("\tFavouring ancient calibrations\n")
+        print("\tChoosing both ancient and random calibrations\n")
         weights = biasedWeights(d_ascending)
         # print("\t\tWeights: ", weights)
         calibrated_nodes_red = nprd.choice(a=list(d_ascending.keys()),
@@ -118,7 +143,6 @@ if __name__ == "__main__":
                            p=weights)
         avg_age_red = computeAvgCalibrationAge(calibrated_nodes_red, id2Height)
         print("Average age of ancient calibrations: ", avg_age_red)
-        print("\tChoosing random calibrations\n")
         weights = uniformWeights(d_ascending)
         calibrated_nodes_blue = nprd.choice(a=list(d_ascending.keys()),
                            size=num_cal,
@@ -135,8 +159,48 @@ if __name__ == "__main__":
                            replace=False,
                            p=weights)
 
-    #print("Calibrated nodes: ", calibrated_nodes_red)
+    ###############################
+    #### OUTPUTING THE CALIBRATIONS
+    ###############################
+    try:
+        fout=open(calibration_file, 'w')
+    except IOError:
+        print ("Unknown file: " + calibration_file)
+        sys.exit()
 
+    try:
+        fout2=open(clade_file, 'w')
+    except IOError:
+        print ("Unknown file: "+ clade_file)
+        sys.exit()
+
+
+    clades_red = list()
+    ages_red =list()
+    clades_blue = list()
+    ages_blue = list()
+
+    print(list(nodeId2LeafListRef.keys()))
+
+    for n in calibrated_nodes_red:
+        clades_red.append(nodeId2LeafListRef[float(n)])
+        ages_red.append(id2Height[n])
+
+    for n in calibrated_nodes_blue:
+        clades_blue.append(nodeId2LeafListRef[float(n)])
+        ages_blue.append(id2Height[n])
+
+    print(len(clades_red))
+    print(len(clades_blue))
+
+
+    outputCalibrations(clades_red, ages_red, fout, fout2, "Old calibrations overrepresented\n")
+    outputCalibrations(clades_blue, ages_blue, fout, fout2, "Random calibrations\n")
+
+
+    ################
+    #### PLOTTING
+    ################
     for node in t.traverse("postorder"):
         if node.name in calibrated_nodes_red:
             node.calibration_red = True
