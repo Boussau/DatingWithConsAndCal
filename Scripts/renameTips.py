2deadd720fb6f92d7625388cf10f39bbb1ad7e9b
BOUSSAU Bastien <bastien.boussau@univ-lyon1.fr>
2020-01-22 13:03:04 +0100

    Script to rename the tips of trees in an anonymous way.

---
+++ Scripts/renameTips.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import sys
+from ete3 import Tree, TreeStyle, NodeStyle
+
+file=sys.argv[1]
+out=file.split('.')[0]+'_renamed.dnd'
+
+
+
+def readTreesFromFile(file):
+    try:
+        f=open(file, 'r')
+    except IOError:
+        print ("Unknown file: "+file)
+        sys.exit()
+
+    trees = list()
+    line = ""
+    for l in f:
+        t = Tree( l.strip())
+        trees.append(t)
+    f.close()
+    return trees
+
+trees = readTreesFromFile(file)
+
+
+nameToNewName = dict()
+
+index = 0
+for leaf in trees[0].iter_leaves():
+    nameToNewName[leaf.name] = "T_"+str(index)
+    index = index + 1
+
+for t in trees:
+    for leaf in t.iter_leaves():
+        leaf.name = nameToNewName[leaf.name]
+
+
+with open(file_output, 'w') as fo:
+    for t in trees:
+        fo.write(t.write(format=1))
