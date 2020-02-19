bb4b6f650a1704386e30aed9d8fde3a992258f7a
BOUSSAU Bastien <bastien.boussau@univ-lyon1.fr>
2020-02-19 16:01:56 +0100

    Silly quote copy-paste bug.

--- launchDatingBalanced_Elte.sh
+++ launchDatingBalanced_Elte.sh
@@ -24,7 +24,7 @@ JOBNAME="balancedRuns"
 MEMORY="1500"
 #in Mb per CPU
 # program to launch
-PROGRAM=“bash balancedNoConstraint.sh”
+PROGRAM="bash balancedNoConstraint.sh"
 
 #### Dating the tree without constraints, with balanced calibrations:
 srun --mem-per-cpu=${MEMORY} --ntasks-per-node=4 -n ${TASKS} -c $THREADS -o ${JOBNAME}_%j.jobout -p $PARTITIONS -J ${JOBNAME}_0 --time $TIME -- $PROGRAM > ${JOBNAME}_%j_rb.out &
