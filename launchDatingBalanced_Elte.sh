59995dc60ab7c33e1a1abf7c03913b9decb9c9bc
BOUSSAU Bastien <bastien.boussau@univ-lyon1.fr>
2020-02-19 10:20:37 +0100

    Change corresponding to the previous commit.

--- launchDatingBalanced_Elte.sh
+++ launchDatingBalanced_Elte.sh
@@ -11,7 +11,7 @@
 # If some error happens before srun it will be outputted to $HOME/nohup.out
 #
 # number of tasks for a file (in same order as files)
-TASKS="4"
+TASKS="1"
 # comma separated list of partitions
 PARTITIONS="high"
 # threads per instance of program needed
@@ -20,14 +20,12 @@ THREADS="1"
 TIME="0"
 # jobname prefix
 JOBNAME="balancedRuns"
-# program to execute
-PROGRAM="rb_icc "
 #
 MEMORY="1500"
 #in Mb per CPU
 
 #### Dating the tree without constraints, with balanced calibrations:
-srun --mem-per-cpu=${MEMORY} --ntasks-per-node=4 -n ${TASKS} -c $THREADS -o ${JOBNAME}_%j.jobout -p $PARTITIONS -J ${JOBNAME}_0 --time $TIME -- echo "tree_file=\"SimulatedTrees/proposedTree.dnd\"; calibration_file=\"Calibrations_10_y_y/proposedTree_calibrations.Rev\" ; constraint_file=\"Constraints/constraints_0.Rev\" ; clade_file=\"Calibrations_10_y_y/proposedTree_clades.Rev\" ; mean_tree_file=\"Alignments/proposedTree_rescaled_altered_unrooted.dnd.fasta.trees_meanBL.nex\" ; var_tree_file=\"Alignments/proposedTree_rescaled_altered_unrooted.dnd.fasta.trees_varBL.nex\"; handle=\"OutputDating/Cal_10_y_y_Cons_0\"; mc3=\"true\"; source(\"Scripts/DatingRevScripts/mainScript.Rev\");" | rb > ${JOBNAME}_%j_rb.out &
+srun --mem-per-cpu=${MEMORY} --ntasks-per-node=4 -n ${TASKS} -c $THREADS -o ${JOBNAME}_%j.jobout -p $PARTITIONS -J ${JOBNAME}_0 --time $TIME -- balancedNoConstraint.sh > ${JOBNAME}_%j_rb.out &
 sleep 1
 
 
