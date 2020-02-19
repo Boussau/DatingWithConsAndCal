9297f4eaf28f17cfb956e10c649246d1e362c9ef
BOUSSAU Bastien <bastien.boussau@univ-lyon1.fr>
2020-02-19 16:43:37 +0100

    5 tasks, not 4

--- launchDatingBalanced_Elte.sh
+++ launchDatingBalanced_Elte.sh
@@ -11,7 +11,7 @@
 # If some error happens before srun it will be outputted to $HOME/nohup.out
 #
 # number of tasks for a file (in same order as files)
-TASKS="4"
+TASKS="5"
 # comma separated list of partitions
 PARTITIONS="high"
 # threads per instance of program needed
