02c4a93e8d3401bf745b655e6f84c678b3cda846
BOUSSAU Bastien <bastien.boussau@univ-lyon1.fr>
2020-02-19 16:37:07 +0100

    Scripts to run all balanced jobs on mellifera.

--- launchDatingBalanced_Elte.sh
+++ launchDatingBalanced_Elte.sh
@@ -24,29 +24,13 @@ JOBNAME="balancedRuns"
 MEMORY="1500"
 #in Mb per CPU
 # program to launch
-PROGRAM="bash balancedNoConstraint.sh"
+PROGRAMS=("bash balancedNoConstraint.sh" "bash balanced1Constraint.sh" "bash balanced5Constraint.sh" "bash balanced10Constraint.sh" "bash balanced15Constraint.sh")
 
 #### Dating the tree without constraints, with balanced calibrations:
-srun --mem-per-cpu=${MEMORY} --ntasks-per-node=4 -n ${TASKS} -c $THREADS -o ${JOBNAME}_%j.jobout -p $PARTITIONS -J ${JOBNAME}_0 --time $TIME -- $PROGRAM > ${JOBNAME}_%j_rb.out &
+for id in "${!PROGRAMS[@]}"; do
+srun --mem-per-cpu=${MEMORY} --ntasks-per-node=4 -n ${TASKS} -c $THREADS -o ${JOBNAME}_%j.jobout -p $PARTITIONS -J ${JOBNAME}_0 --time $TIME -- ${PROGRAMS[$id]} > ${JOBNAME}_${PROGRAMS[$id]}_%j_rb.out &
 sleep 1
-
+done
 
 
 # wait for sruns to finish
-
-#
-#
-# #### Dating the tree without constraints, with balanced calibrations:
-# echo "tree_file=\"SimulatedTrees/proposedTree.dnd\"; calibration_file=\"Calibrations_10_y_y/proposedTree_calibrations.Rev\" ; constraint_file=\"Constraints/constraints_0.Rev\" ; clade_file=\"Calibrations_10_y_y/proposedTree_clades.Rev\" ; mean_tree_file=\"Alignments/proposedTree_rescaled_altered_unrooted.dnd.fasta.trees_meanBL.nex\" ; var_tree_file=\"Alignments/proposedTree_rescaled_altered_unrooted.dnd.fasta.trees_varBL.nex\"; handle=\"OutputDating/Cal_10_y_y_Cons_0\"; source(\"Scripts/DatingRevScripts/mainScript.Rev\");" | rb
-#
-# #### Dating the tree with 1 constraint, with balanced calibrations:
-# echo "tree_file=\"SimulatedTrees/proposedTree.dnd\"; calibration_file=\"Calibrations_10_y_y/proposedTree_calibrations.Rev\" ; constraint_file=\"Constraints/constraints_1.Rev\" ; clade_file=\"Calibrations_10_y_y/proposedTree_clades.Rev\" ; mean_tree_file=\"Alignments/proposedTree_rescaled_altered_unrooted.dnd.fasta.trees_meanBL.nex\" ; var_tree_file=\"Alignments/proposedTree_rescaled_altered_unrooted.dnd.fasta.trees_varBL.nex\"; handle=\"OutputDating/Cal_10_y_y_Cons_1\"; source(\"Scripts/DatingRevScripts/mainScript.Rev\");" | rb
-#
-# #### Dating the tree with 5 constraints, with balanced calibrations:
-# echo "tree_file=\"SimulatedTrees/proposedTree.dnd\"; calibration_file=\"Calibrations_10_y_y/proposedTree_calibrations.Rev\" ; constraint_file=\"Constraints/constraints_5.Rev\" ; clade_file=\"Calibrations_10_y_y/proposedTree_clades.Rev\" ; mean_tree_file=\"Alignments/proposedTree_rescaled_altered_unrooted.dnd.fasta.trees_meanBL.nex\" ; var_tree_file=\"Alignments/proposedTree_rescaled_altered_unrooted.dnd.fasta.trees_varBL.nex\"; handle=\"OutputDating/Cal_10_y_y_Cons_5\"; source(\"Scripts/DatingRevScripts/mainScript.Rev\");" | rb
-#
-# #### Dating the tree with 10 constraints, with balanced calibrations:
-# echo "tree_file=\"SimulatedTrees/proposedTree.dnd\"; calibration_file=\"Calibrations_10_y_y/proposedTree_calibrations.Rev\" ; constraint_file=\"Constraints/constraints_10.Rev\" ; clade_file=\"Calibrations_10_y_y/proposedTree_clades.Rev\" ; mean_tree_file=\"Alignments/proposedTree_rescaled_altered_unrooted.dnd.fasta.trees_meanBL.nex\" ; var_tree_file=\"Alignments/proposedTree_rescaled_altered_unrooted.dnd.fasta.trees_varBL.nex\"; handle=\"OutputDating/Cal_10_y_y_Cons_10\"; source(\"Scripts/DatingRevScripts/mainScript.Rev\");" | rb
-#
-# #### Dating the tree with 15 constraints, with balanced calibrations:
-# echo "tree_file=\"SimulatedTrees/proposedTree.dnd\"; calibration_file=\"Calibrations_10_y_y/proposedTree_calibrations.Rev\" ; constraint_file=\"Constraints/constraints_15.Rev\" ; clade_file=\"Calibrations_10_y_y/proposedTree_clades.Rev\" ; mean_tree_file=\"Alignments/proposedTree_rescaled_altered_unrooted.dnd.fasta.trees_meanBL.nex\" ; var_tree_file=\"Alignments/proposedTree_rescaled_altered_unrooted.dnd.fasta.trees_varBL.nex\"; handle=\"OutputDating/Cal_10_y_y_Cons_15\"; source(\"Scripts/DatingRevScripts/mainScript.Rev\");" | rb
