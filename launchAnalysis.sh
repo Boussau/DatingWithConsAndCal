24c74028c85890e52e5927ff934c7672d2dd3605
BOUSSAU Bastien <bastien.boussau@univ-lyon1.fr>
2020-03-02 15:39:18 +0100

    Updated analysis of reconstructed trees.

--- launchAnalysis.sh
+++ launchAnalysis.sh
@@ -1,11 +1,14 @@
-python Scripts/analyzeMAPTree.py SimulatedTrees/proposedTree.dnd OutputDating/Cal_10_n_y_Cons_0_BD_WNr_BL.tree
-python Scripts/analyzeMAPTree.py SimulatedTrees/proposedTree.dnd OutputDating/Cal_10_n_y_Cons_1_cons_BD_WNr_BL.tree
-python Scripts/analyzeMAPTree.py SimulatedTrees/proposedTree.dnd OutputDating/Cal_10_n_y_Cons_5_cons_BD_WNr_BL.tree
-python Scripts/analyzeMAPTree.py SimulatedTrees/proposedTree.dnd OutputDating/Cal_10_n_y_Cons_10_cons_BD_WNr_BL.tree
-python Scripts/analyzeMAPTree.py SimulatedTrees/proposedTree.dnd OutputDating/Cal_10_n_y_Cons_15_cons_BD_WNr_BL.tree
+python Scripts/analyzeMAPTree.py SimulatedTrees/proposedTree.dnd OutputDating/Cal_10_n_y_Cons_0_cons_BD_UGAMr_BL_MC3noMix.trees.tree y
+python Scripts/analyzeMAPTree.py SimulatedTrees/proposedTree.dnd OutputDating/Cal_10_n_y_Cons_1_cons_BD_UGAMr_BL_MC3noMix.trees.tree y
+python Scripts/analyzeMAPTree.py SimulatedTrees/proposedTree.dnd OutputDating/Cal_10_n_y_Cons_5_cons_BD_UGAMr_BL_MC3noMix.trees.tree y
+python Scripts/analyzeMAPTree.py SimulatedTrees/proposedTree.dnd OutputDating/Cal_10_n_y_Cons_10_cons_BD_UGAMr_BL_MC3noMix.trees.tree y
+python Scripts/analyzeMAPTree.py SimulatedTrees/proposedTree.dnd OutputDating/Cal_10_n_y_Cons_15_cons_BD_UGAMr_BL_MC3noMix.trees.tree y
 
-python Scripts/analyzeMAPTree.py SimulatedTrees/proposedTree.dnd OutputDating/Cal_10_y_y_Cons_0_BD_WNr_BL.tree
-python Scripts/analyzeMAPTree.py SimulatedTrees/proposedTree.dnd OutputDating/Cal_10_y_y_Cons_1_cons_BD_WNr_BL.tree
-python Scripts/analyzeMAPTree.py SimulatedTrees/proposedTree.dnd OutputDating/Cal_10_y_y_Cons_5_cons_BD_WNr_BL.tree
-python Scripts/analyzeMAPTree.py SimulatedTrees/proposedTree.dnd OutputDating/Cal_10_y_y_Cons_10_cons_BD_WNr_BL.tree
-python Scripts/analyzeMAPTree.py SimulatedTrees/proposedTree.dnd OutputDating/Cal_10_y_y_Cons_15_cons_BD_WNr_BL.tree
+python Scripts/analyzeMAPTree.py SimulatedTrees/proposedTree.dnd OutputDating/Cal_10_y_y_Cons_0_cons_BD_UGAMr_BL_MC3noMix.trees.tree y
+python Scripts/analyzeMAPTree.py SimulatedTrees/proposedTree.dnd OutputDating/Cal_10_y_y_Cons_1_cons_BD_UGAMr_BL_MC3noMix.trees.tree y
+python Scripts/analyzeMAPTree.py SimulatedTrees/proposedTree.dnd OutputDating/Cal_10_y_y_Cons_5_cons_BD_UGAMr_BL_MC3noMix.trees.tree y
+python Scripts/analyzeMAPTree.py SimulatedTrees/proposedTree.dnd OutputDating/Cal_10_y_y_Cons_15_cons_BD_UGAMr_BL_MC3noMix.trees.tree y
+
+
+
+####python Scripts/analyzeMAPTree.py SimulatedTrees/proposedTree.dnd OutputDating/Cal_10_y_y_Cons_15_cons_BD_WNr_BL.tree
