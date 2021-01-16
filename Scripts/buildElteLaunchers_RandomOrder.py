fd9cbd5570e59eda2c808b8cd7191b86123c97f9
BOUSSAU Bastien <bastien.boussau@univ-lyon1.fr>
2021-01-16 21:39:23 +0100

    needed bash call

--- Scripts/buildElteLaunchers_RandomOrder.py
+++ Scripts/buildElteLaunchers_RandomOrder.py
@@ -21,9 +21,9 @@ for i in range(num):
     for j in range(1, 15):
         # write down 1 string for the balanced case,
         # and 1 string for the unbalanced case.
-        all_scripts_balanced = all_scripts_balanced + " \"" + \
+        all_scripts_balanced = all_scripts_balanced + " \"bash " + \
             os.path.join(out_i, "balanced_" + str(j) + ".sh\"")
-        all_scripts_unbalanced = all_scripts_unbalanced + " \"" + \
+        all_scripts_unbalanced = all_scripts_unbalanced + " \"bash " + \
             os.path.join(out_i, "unbalanced_" + str(j) + ".sh\"")
     with open(os.path.join(folder_with_launchers, "launcher_Rep_" + str(i) + ".sh"), "w") as fout:
         fout.write(lines.replace("\"TOREPLACEWITH28JOBS\"", all_scripts_balanced +
