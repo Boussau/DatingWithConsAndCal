5f8f51828b2bc7cf28f4c0e50f14e59a3a7f9b23
BOUSSAU Bastien <bastien.boussau@univ-lyon1.fr>
2021-01-19 10:03:02 +0100

    Improved slightly elte launchers.

--- FolderWithLaunchersRandomOrder/launcher_Rep_5.sh
+++ FolderWithLaunchersRandomOrder/launcher_Rep_5.sh
@@ -13,7 +13,7 @@
 # number of tasks for a file (in same order as files)
 TASKS=("1" "1" "1" "1" "1" "1" "1" "1" "1" "1" "1" "1" "1" "1" "1" "1" "1" "1" "1" "1" "1" "1" "1" "1" "1" "1" "1" "1")
 # comma separated list of partitions
-PARTITIONS="normal"
+PARTITIONS="med"
 # threads per instance of program needed
 THREADS="4"
 # expected runtime
@@ -28,7 +28,7 @@ PROGRAMS=( "bash RandomConstraintOrders/Rep_5/balanced_1.sh" "bash RandomConstra
 
 #### Dating the tree without constraints, with balanced calibrations:
 for id in "${!PROGRAMS[@]}"; do
-srun --mem-per-cpu=${MEMORY} --ntasks-per-node=4 -n ${TASKS} -c $THREADS -o ${JOBNAME}_%j.jobout -p $PARTITIONS -J ${JOBNAME}_0 --time $TIME -- ${PROGRAMS[$id]} > ${JOBNAME}_$id_%j_rb.out &
+srun --mem-per-cpu=${MEMORY} -n ${TASKS} -c $THREADS -o ${JOBNAME}_%j.jobout -p $PARTITIONS -J ${JOBNAME}_0 --time $TIME -- ${PROGRAMS[$id]} > ${JOBNAME}_$id_%j_rb.out &
 sleep 1
 done
 
