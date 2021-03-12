476ac52b25c449047a8a1e93d5baddc91b3b5b30
BOUSSAU Bastien <bastien.boussau@univ-lyon1.fr>
2021-03-12 14:23:40 +0100

    Some cleaning

--- proximalDistal/uninfoBalanced.sh
+++
@@ -1,4 +0,0 @@
-
-#### Dating the tree with 5 uninformative constraints, with balanced calibrations:
-echo "tree_file=\"SimulatedTrees/proposedTree.dnd\"; calibration_file=\"Calibrations_10_y_y/proposedTree_calibrations.Rev\" ; constraint_file=\"Constraints/constraints_uninformative.Rev\" ; clade_file=\"Calibrations_10_y_y/proposedTree_clades.Rev\" ; mean_tree_file=\"Alignments/proposedTree_rescaled_altered_unrooted.dnd.fasta.trees_meanBL.nex\" ; var_tree_file=\"Alignments/proposedTree_rescaled_altered_unrooted.dnd.fasta.trees_varBL.nex\"; handle=\"OutputDatingInfoUninfo/Cal_10_y_y_Cons_uninfo\"; rate_model=\"UGAMr\"; mc3=\"true\"; source(\"Scripts/DatingRevScripts/mainScript.Rev\");" | /scratch/local/bin/rb-mpi_icc
-
